721dda090a78aa69d22b0b9e3a9e87b01ee0c843
BipinKalra <bipinkalra@gmail.com>
2021-03-02 13:13:01 +0530

    updates

--- src/index.js
+++ src/index.js
@@ -7,16 +7,20 @@ import ReactDOM from "react-dom";
 
 // Create a React component
 const App = () => {
-  const buttonText = "Click Me!"
+  const buttonText = "Click Me!";
+  // const buttonText = {
+  //   text: "Click Me!",
+  // };
+  // This gives an error - javascript objects can't be reduced to text inside jsx
 
   return (
     <div>
-      <label className="label" for="name">
+      <label className="label" htmlFor="name">
         Enter Name
       </label>
       <input type="text" id="name" />
       <button style={{ backgroundColor: "blue", color: "white" }}>
-        { buttonText }
+        {buttonText}
       </button>
     </div>
   );
